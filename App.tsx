616c42e17324c8c7d0d956b5e9cc3ea6a5d350a0
josearicapa <54639293+josearicapa@users.noreply.github.com>
2023-04-05 05:53:00 -0500

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import Counter from './src/Counter';
 import NumberList from './src/NumberListMemo';
-
 import './style.css';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: blue;
+`;
 
 export default function App() {
   const [numbers, setNumbers] = React.useState([1, 2, 3]);
@@ -25,7 +29,7 @@ export default function App() {
   return (
     <React.Fragment>
       <NumberList numbers={numbers} />
-      <button onClick={addNumber}>Add number</button>
+      <Button onClick={addNumber}>Add number</Button>
       <button onClick={removeNumber}>Remove number</button>
       <Counter />
     </React.Fragment>
